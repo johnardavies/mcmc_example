f556285f9ef6edd382f39f31efe21216191170f7
johnardavies <johnardavies@gmail.com>
2024-07-28 07:56:30 +0100

    Update mcmc_gamma_example.py multi chart

--- mcmc_gamma_example.py
+++ mcmc_gamma_example.py
@@ -2,20 +2,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import norm
-from scipy.special import gamma 
-
+from scipy.special import gamma
 from scipy.stats import gamma as gammadist
 
 
-
 # Number of samples drawn
-sample_number=100000
+sample_number = 100000
 
 # Functions that we are using
 
+
 def poly_want_to_sample(x):
-    """The polynomial we want to sample - known up to the normalising constant """
+    """The polynomial we want to sample - known up to the normalising constant"""
     return 0.3 * np.exp(-0.2 * x * x) + 0.7 * np.exp(-0.2 * (x - 10) * (x - 10))
+
 
 def posterior(lambda_val, alpha, beta, data):
     """function that gives the values of the gamma posterior for different values of alpha, beta and data"""
@@ -26,7 +26,7 @@ def posterior(lambda_val, alpha, beta, data):
     new_alpha = alpha + sum_val
 
     numerator = (
-        (new_beta ** new_alpha)
+        (new_beta**new_alpha)
         * np.exp(-lambda_val * new_beta)
         * (lambda_val ** (new_alpha - 1))
     )
@@ -36,77 +36,93 @@ def posterior(lambda_val, alpha, beta, data):
 
     return val
 
-data_var=list(np.random.poisson(6, 2))
 
-def mcmc_threshold(x, y):
-    """Function that generates the mcmc probaility threshold for accepting the proposed state
-         x is the current state
-         y is the proposed new state
-         norm.pdf(x,y,100) is the function x of a normal distribution with mean y and variance 100
-         """
-    ratio = (posterior(y,0.3,0.1, data_var) * norm.pdf(x, y, 100)) / (
-       posterior(x,0.3,0.1, data_var) * norm.pdf(y, x, 100)
+data_sets = [0, 3, 5, 25]
+
+
+def mcmc_threshold(x, y, data_sample):
+    """Function thao t generates the mcmc probaility threshold for accepting the proposed state
+    x is the current state
+    y is the proposed new s	tate
+    norm.pdf(x,y,100) is the function x of a normal distribution with mean y and variance 100
+    """
+    ratio = (posterior(y, 0.3, 0.1, data_sample) * norm.pdf(x, y, 100)) / (
+        posterior(x, 0.3, 0.1, data_sample) * norm.pdf(y, x, 100)
     )
 
-    return   min(ratio, 1)
-
-# Create a data frame to hold the results
-data_store = pd.DataFrame(index=range(sample_number), columns=["state_of_chain"])
+    return min(ratio, 1)
 
 
-# Create an initial value to start the chain
-initial_value=np.random.normal(10,10)
+fig, axs = plt.subplots(4, figsize=(12, 15))
 
-state = initial_value
+for index, sample_size in enumerate(data_sets):
+    # Create a data frame to hold the results
+    data_store = pd.DataFrame(index=range(sample_number), columns=["state_of_chain"])
 
-# Run the samples
-for i in range(0, sample_number):
-    # Generate the proposed state
-    prop_value = np.random.normal(state, 100)
-
-    # Generate a number between 1 and 0 to simulate probability of acceptance
-    prob_sample = np.random.uniform(0, 1)
-
-    # Condition for accepting the proposed state 
-    # the probability of the state being accepted is prob_sample
-    if prob_sample < mcmc_threshold(state, prop_value):
-        state = prop_value
+    if index == 0:
+        values = []
     else:
-        state = state
-    # Storing the value in the dataframe
-    data_store["state_of_chain"].loc[i] = state
-    # Prints charts at regular intervals
-    if i % 10000 == 0:
-        plt.figure(figsize=(14, 10))
-        plt.xlim(0, 15)
-        plt.hist(data_store["state_of_chain"], density=True, bins=50)
-        plt.title("Distribution of mcmc chain's " + str(i) + "'th run", weight="bold")
-        x = np.arange(0, 15, 0.1)
-        # Plots the probability distribution we want to sample scaled to chart
-        plt.plot(x, posterior(x,0.3,0.1,data_var)*0.2, color="red")
+        values = list(np.random.poisson(6, sample_size))
+    # Create an initial value to start the chain
+    initial_value = np.random.normal(10, 10)
 
-data_mean=data_store["state_of_chain"].mean()
-data_var=data_store["state_of_chain"].var()
+    state = initial_value
 
-alpha_est=(data_mean**2)/(data_var**2)
-beta_est=(data_mean)/(data_var**2)
+    # Run the samples
+    for i in range(0, sample_number):
+        # Generate the proposed state
+        prop_value = np.random.normal(state, 100)
 
-#gamma.pdf(x,alpha_est,loc=0,scale=(1/beta_est))
+        # Generate a number between 1 and 0 to simulate probability of acceptance
+        prob_sample = np.random.uniform(0, 1)
 
-plt.figure(figsize=(14, 10))
-plt.xlim(0, 15)
-plt.hist(data_store["state_of_chain"], density=True, bins=50)
-plt.title("Distribution of mcmc chain's " + str(i) + "'th run", weight="bold")
-x = np.arange(0, 15, 0.1)
- # Plots the probability distribution we want to sample scaled to chart
-#plt.plot(x, posterior(x,0.3,0.1,data_var)*0.2, color="red")
-plt.plot(x, gammadist.pdf(x,alpha_est,loc=0,scale=(1/beta_est)), color="blue")
-print(plt.show())
+        # Condition for accepting the proposed state
+        # the probability of the state being accepted is prob_sample
+        if prob_sample < mcmc_threshold(state, prop_value, values):
+            state = prop_value
+        else:
+            state = state
+        # Storing the value in the dataframe
+        data_store["state_of_chain"].loc[i] = state
+        # Prints charts at regular intervals
+        if i % 10000 == 0:
+            plt.figure(figsize=(14, 10))
+            plt.xlim(0, 15)
+            plt.hist(data_store["state_of_chain"], density=True, bins=50)
+            plt.title(
+                "Distribution of mcmc chain's " + str(i) + "'th run", weight="bold"
+            )
+            x = np.arange(0, 15, 0.1)
+            # Plots the probability distribution we want to sample scaled to chart
+            # plt.plot(x, posterior(x,0.3,0.1,data_var)*0.2, color="red")
+
+    data_mean = data_store["state_of_chain"].mean()
+    data_var = data_store["state_of_chain"].var()
+
+    alpha_est = (data_mean**2) / (data_var**2)
+    beta_est = (data_mean) / (data_var**2)
+
+    axs[index].hist(
+        data_store["state_of_chain"], density=True, bins=50, label="mcmc distribution"
+    )
+    axs[index].legend()
+    axs[index].set_title(
+        f"Distribution of mcmc chain's {i}'th run based on {sample_size} datapoints",
+        weight="bold",
+    )
+
+    x = np.arange(0, 15, 0.1)
+    # Plots the probability distribution we want to sample scaled to chart
+    axs[index].plot(
+        x, posterior(x, 0.3, 0.1, values), color="yellow", label="posterior"
+    )
+    axs[index].plot(
+        x,
+        gammadist.pdf(x, alpha_est, loc=0, scale=(1 / beta_est)),
+        color="blue",
+        label="fitted distribution",
+    )
 
 
-
-
-
-print(plt.show()) 
-plt.savefig("figurename.png")
+fig.savefig("figurename.png")
 plt.close()
