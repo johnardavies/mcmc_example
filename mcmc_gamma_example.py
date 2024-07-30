ec80a82269060cef02429066bf84cc0bf15b61bd
johnardavies <johnardavies@gmail.com>
2024-07-30 18:59:27 +0100

    Update mcmc_gamma_example.py

--- mcmc_gamma_example.py
+++ mcmc_gamma_example.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+from scipy.stats import norm, lognorm
 from scipy.special import gamma
 from scipy.stats import gamma as gammadist
 
@@ -37,23 +37,22 @@ def posterior(lambda_val, alpha, beta, data):
     return val
 
 
-data_sets = [0, 3, 5, 25]
-
-
 def mcmc_threshold(x, y, data_sample):
     """Function thao t generates the mcmc probaility threshold for accepting the proposed state
     x is the current state
-    y is the proposed new s	tate
+    y is the proposed new state
     norm.pdf(x,y,100) is the function x of a normal distribution with mean y and variance 100
     """
-    ratio = (posterior(y, 0.3, 0.1, data_sample) * norm.pdf(x, y, 100)) / (
-        posterior(x, 0.3, 0.1, data_sample) * norm.pdf(y, x, 100)
+    ratio = (posterior(y, 0.3, 0.1, data_sample) * norm.pdf(x, y, 10)) / (
+        posterior(x, 0.3, 0.1, data_sample) * norm.pdf(y, x, 10)
     )
 
     return min(ratio, 1)
 
 
+data_sets = [0, 3, 5, 25]
 fig, axs = plt.subplots(4, figsize=(12, 15))
+x = np.arange(0, 15, 0.1)
 
 for index, sample_size in enumerate(data_sets):
     # Create a data frame to hold the results
@@ -67,14 +66,18 @@ for index, sample_size in enumerate(data_sets):
     initial_value = np.random.normal(10, 10)
 
     state = initial_value
+    
 
     # Run the samples
     for i in range(0, sample_number):
         # Generate the proposed state
-        prop_value = np.random.normal(state, 100)
+        prop_value = np.random.normal(state, 10)
 
         # Generate a number between 1 and 0 to simulate probability of acceptance
-        prob_sample = np.random.uniform(0, 1)
+        prob_sample = abs(np.random.uniform(0, 1))
+
+        if prop_value <  0:
+           prop_value = -prop_value
 
         # Condition for accepting the proposed state
         # the probability of the state being accepted is prob_sample
@@ -84,45 +87,40 @@ for index, sample_size in enumerate(data_sets):
             state = state
         # Storing the value in the dataframe
         data_store["state_of_chain"].loc[i] = state
-        # Prints charts at regular intervals
-        if i % 10000 == 0:
-            plt.figure(figsize=(14, 10))
-            plt.xlim(0, 15)
-            plt.hist(data_store["state_of_chain"], density=True, bins=50)
-            plt.title(
-                "Distribution of mcmc chain's " + str(i) + "'th run", weight="bold"
-            )
-            x = np.arange(0, 15, 0.1)
-            # Plots the probability distribution we want to sample scaled to chart
-            # plt.plot(x, posterior(x,0.3,0.1,data_var)*0.2, color="red")
 
+    # calculate the mean and the variance and from that calculate alpha and beta
     data_mean = data_store["state_of_chain"].mean()
+
     data_var = data_store["state_of_chain"].var()
 
     alpha_est = (data_mean**2) / (data_var**2)
     beta_est = (data_mean) / (data_var**2)
 
+
+    # chart the results of each run
     axs[index].hist(
-        data_store["state_of_chain"], density=True, bins=50, label="mcmc distribution"
+        data_store["state_of_chain"],
+        density=True,
+        bins=50,
+        label=f"mcmc distribution mean={data_mean:.2f}",
     )
-    axs[index].legend()
+
     axs[index].set_title(
-        f"Distribution of mcmc chain's {i}'th run based on {sample_size} datapoints",
+        f"Distribution of 10,000 mcmc chain runs based on {sample_size} datapoints",
         weight="bold",
     )
 
-    x = np.arange(0, 15, 0.1)
+   
     # Plots the probability distribution we want to sample scaled to chart
     axs[index].plot(
         x, posterior(x, 0.3, 0.1, values), color="yellow", label="posterior"
     )
     axs[index].plot(
         x,
-        gammadist.pdf(x, alpha_est, loc=0, scale=(1 / beta_est)),
+        gammadist.pdf(x, alpha_est, loc=0, scale=1/beta_est),
         color="blue",
         label="fitted distribution",
     )
+    axs[index].legend()
 
-
-fig.savefig("figurename.png")
-plt.close()
+fig.savefig("mcmc_chart.png")
